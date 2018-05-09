84486e787b3b48b98a9857b81a9b91e181749a5e
Jan C. Rivenaes <JRIV@statoil.com>
2018-05-09 07:33:23 +0200

    Added .gitignore

---
+++ src/fmu_config/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fmu_config"""
+
+__author__ = """Jan C. Rivenaes"""
+__email__ = 'jriv@statoil.com'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
