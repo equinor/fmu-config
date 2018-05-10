57767a1261a03fac50061740776c09a6bd25f14c
Jan C. Rivenaes <JRIV@statoil.com>
2018-05-10 16:30:41 +0200

    Improving various config steps

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,12 @@
 import fmu_config
+from xtgeo.common import XTGeoDialog
 
 tfile1 = 'tests/data/yaml/troll1/global_variables.yaml'
 rfile1 = 'tests/data/yaml/reek1/global_variables.yaml'
+
+xxx = XTGeoDialog()
+logger = xxx.basiclogger(__name__)
+testdir = xxx.testsetup()
 
 
 def test_basic_troll():
@@ -18,7 +23,7 @@ def test_basic_troll():
     assert len(cfg.config['horizons']) == 6
 
     # export the config as a global variables IPL
-    cfg.to_ipl('myfile')
+    cfg.to_ipl(testdir + '/myfile.ipl')
 
 
 def test_basic_reek():
@@ -31,6 +36,10 @@ def test_basic_reek():
     cfg.parse(rfile1)
 
     cfg.show()
+
+    # will write the eclipse files
+    cfg.to_eclipse()
+    cfg.to_ipl()
 
     # assert len(cfg.config['horizons']) == 6
 
