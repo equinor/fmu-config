4bfa80af1d46f00afeaffc7a03660755469f9787
Jan C. Rivenaes <JRIV@statoil.com>
2018-08-02 08:21:08 +0200

    Adding more test files (Troll)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+import os.path
+
 import fmu_config
 import fmu_config.fmuconfigrunner as rr
 
 from xtgeo.common import XTGeoDialog
 
 tfile1 = 'tests/data/yaml/troll1/global_variables.yaml'
+tfile2 = 'tests/data/yaml/troll2/global_master_config.yml'
 rfile1 = 'tests/data/yaml/reek1/global_variables.yaml'
 
 xxx = XTGeoDialog()
 logger = xxx.basiclogger(__name__)
-testdir = xxx.testsetup()
+testdir = 'TMP'
 
 
 def test_basic_troll():
@@ -28,6 +35,24 @@ def test_basic_troll():
     # export the config as a global variables IPL
     # cfg.to_ipl(testdir + '/myfile.ipl')
 
+
+def test_basic_troll2():
+    """Test basic behaviour"""
+
+    cfg = fmu_config.ConfigParserFMU()
+
+    assert isinstance(cfg, fmu_config.ConfigParserFMU)
+
+    cfg.parse(tfile2)
+
+    cfg.show()
+    cfg.show(style='json')
+
+#    assert len(cfg.config['horizons']) == 6
+
+    # export the config as a global variables IPL
+    logger.info('Test dir is {}'.format(testdir))
+    cfg.to_ipl(destination=os.path.join(testdir, 'myfile.ipl'))
 
 def test_basic_reek():
     """Test basic behaviour, Reek setup"""
