b54f2f29453e02c794fd47464254227e367c11b3
Jan C. Rivenaes <JRIV@statoil.com>
2018-08-08 14:05:18 +0200

    Iterating on format for uncertainty params and IPL. Still in eval.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-
-from __future__ import division, absolute_import
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
 
 import os.path
 
 from fmu import config
-import fmu.config.fmuconfigrunner as rr
+# import fmu.config.fmuconfigrunner as fmurun
 
 from xtgeo.common import XTGeoDialog
 
-tfile1 = 'tests/data/yaml/troll1/global_variables.yaml'
+tfile1 = 'tests/data/yaml/troll1/global_variables.yml'
 tfile2 = 'tests/data/yaml/troll2/global_master_config.yml'
-rfile1 = 'tests/data/yaml/reek1/global_variables.yaml'
+rfile1 = 'tests/data/yaml/reek1/global_variables.yml'
 
 xxx = XTGeoDialog()
 logger = xxx.basiclogger(__name__)
@@ -38,9 +38,6 @@ def test_basic_troll():
 
     assert len(cfg.config['horizons']) == 6
 
-    # export the config as a global variables IPL
-    # cfg.to_ipl(testdir + '/myfile.ipl')
-
 
 def test_basic_troll2():
     """Test basic behaviour"""
@@ -54,44 +51,43 @@ def test_basic_troll2():
     cfg.show()
     cfg.show(style='json')
 
-#    assert len(cfg.config['horizons']) == 6
-
     # export the config as a global variables IPL
     logger.info('Test dir is {}'.format(testdir))
-    cfg.to_ipl(destination=os.path.join(testdir, 'myfile.ipl'))
+    cfg.to_ipl(destination=os.path.join(testdir),
+               template=os.path.join(testdir))
 
 
-def test_basic_reek():
-    """Test basic behaviour, Reek setup"""
+# def test_basic_reek():
+#     """Test basic behaviour, Reek setup"""
 
-    cfg = config.ConfigParserFMU()
+#     cfg = config.ConfigParserFMU()
 
-    assert isinstance(cfg, config.ConfigParserFMU)
+#     assert isinstance(cfg, config.ConfigParserFMU)
 
-    cfg.parse(rfile1)
+#     cfg.parse(rfile1)
 
-    cfg.show()
+#     cfg.show()
 
-    # will write the eclipse files
-    cfg.to_eclipse()
-    cfg.to_ipl()
-    cfg.to_yaml('myyaml', destination='TMP')
-    cfg.to_json('myjson', destination='TMP')
+#     # will write the eclipse files
+#     cfg.to_eclipse()
+#     cfg.to_ipl()
+#     cfg.to_yaml('myyaml', destination='TMP')
+#     cfg.to_json('myjson', destination='TMP')
 
-    # assert len(cfg.config['horizons']) == 6
+#     # assert len(cfg.config['horizons']) == 6
 
-    # # export the config as a global variables IPL
-    # cfg.to_ipl('myfile')
-
-
-def test_command_make_ipls():
-    """Make IPL both global_variable.ipl and global_variables.tmpl, Reek."""
-    rr.main(['--input', rfile1, '--mode', 'ipl'])  # noqa
+#     # # export the config as a global variables IPL
+#     # cfg.to_ipl('myfile')
 
 
-def test_command_make_yamls():
-    """Make IPL both global_variables_rms.yml and global_variables_tmpl.yml,
-    Reek.
-    """
+# def test_command_make_ipls():
+#     """Make IPL both global_variable.ipl and global_variables.tmpl, Reek."""
+#     fmurun.main(['--input', rfile1, '--mode', 'ipl'])  # noqa
 
-    rr.main(['--input', rfile1, '--mode', 'yaml', --tool, 'rms'])  # noqa
+
+# def test_command_make_yamls():
+#     """Make IPL both global_variables_rms.yml and global_variables_tmpl.yml,
+#     Reek.
+#     """
+
+#     fmurun.main(['--input', rfile1, '--mode', 'yaml', --tool, 'rms'])  # noqa
