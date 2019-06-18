30fe7ebc5a516aa7f0991639a5bb2fb27d596c86
Jan C. Rivenaes <JRIV@statoil.com>
2019-06-18 12:34:19 +0200

    Replaced versioneeer with setuptools_scm

--- setup.py
+++ setup.py
@@ -2,102 +2,63 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
-from glob import glob
+import os
 from os.path import basename
 from os.path import splitext
+from glob import glob
 
 from setuptools import setup, find_packages
-import versioneer
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [
-]
+requirements = []
 
 # for 'python setup.py test' to work; need pytest runner:
-setup_requirements = [
-    'pytest-runner',
-]
+setup_requirements = ["pytest-runner", "setuptools_scm>=3.2.0"]
 
-test_requirements = [
-    'pytest',
-]
+test_requirements = ["pytest"]
 
-# entry points setting; not is use...
-fmuconfig_function = ('fmuconfig='
-                      'fmu.config.fmuconfigrunner:main')
-# -----------------------------------------------------------------------------
-# Explaining versions:
-# As system the PEP 440 major.minor.micro is used:
-# - major: API or any very larger changes
-# - minor: Functionality added, mostly backward compatibility but some
-#          functions may change. Also includes larger refactoring of code.
-# - micro: Added functionality and bug fixes with no expected side effects
-# - Provide a tag on the form 3.4.0 for each release!
-#
-# Hence, use major.minor.micro
-# -----------------------------------------------------------------------------
+# entry points setting
+fmuconfig_function = "fmuconfig=" "fmu.config.fmuconfigrunner:main"
 
 
-def the_version():
-    """Process the version, to avoid non-pythonic version schemes.
-
-    Means that e.g. 1.5.12+2.g191571d.dirty is turned to 1.5.12.2.dev0
-
-    This function must be ~identical to xtgeo._theversion.py
-    """
-
-    version = versioneer.get_version()
-    sver = version.split('.')
-    print('\nFrom TAG description: {}'.format(sver))
-
-    useversion = 'UNSET'
-    if len(sver) == 3:
-        useversion = version
-    else:
-        bugv = sver[2].replace('+', '.')
-
-        if 'dirty' in version:
-            ext = '.dev0'
-        else:
-            ext = ''
-        useversion = '{}.{}.{}{}'.format(sver[0], sver[1], bugv, ext)
-
-    print('Using version {}\n'.format(useversion))
-    return useversion
+def src(x):
+    root = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(root, x))
 
 
 setup(
-    name='fmu_config',
-    version=the_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    name="fmu_config",
+    use_scm_version={"root": src(""), "write_to": src("src/fmu/config/_theversion.py")},
     description="Library for various config scripts in FMU scope",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     author="Jan C. Rivenaes",
-    author_email='jriv@statoil.com',
-    url='https://git.equinor.com/fmu-utilities/fmu-config',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author_email="jriv@equinor.com",
+    url="https://git.equinor.com/fmu-utilities/fmu-config",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
-    keywords='fmu, config',
+    keywords="fmu, config",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     setup_requires=setup_requirements,
 )
