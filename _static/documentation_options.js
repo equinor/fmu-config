48ca3aab9e7d006aefddd06cf18aa9584c6c3603
fmu-config-github-action <fmu-config-github-action>
2022-02-15 09:15:47 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g6aee329',
+    VERSION: '0.1.dev1+g65e7ace',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
