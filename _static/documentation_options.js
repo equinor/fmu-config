6722ddf7d3f724998ff8603157b335e0793a4a0a
fmu-config-github-action <fmu-config-github-action>
2025-12-23 00:31:41 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.6.1.dev2+g0a63b1ba0.d20251222',
+    VERSION: '1.6.1.dev2+g0a63b1ba0.d20251223',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
